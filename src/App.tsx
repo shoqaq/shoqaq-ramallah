5471a1fd2181a5ba8cd468e382a4bb57566c4b0f
shoqaq <shoqaq.ramallah@gmail.com>
2026-04-08 13:20:09 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import globals from 'globals'
+
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
